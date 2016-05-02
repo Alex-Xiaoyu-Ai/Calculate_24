1cda8769bab105499db9b04c93650da4ffe01baa
Xiaoyu Ai <xiaoyu@xiaoyus-macbook-pro.local>
2016-05-02 17:03:17 +1000

    Initial Commit

---
+++ Calculate_24_Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calculate_24_Algorithm
+//
+//  Created by Xiaoyu Ai on 2/05/2016.
+//  Copyright © 2016 Xiaoyu Ai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
